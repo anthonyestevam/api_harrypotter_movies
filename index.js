82b44a9a96ddfb8ccfc32b1ced9bfcfe402d5124
anthonyestevam <twes881@gmail.com>
2024-10-11 17:10:12 -0300

    atualizando projeto

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const yaml = require('yamljs');
+const swaggerDocument = yaml.load('./swagger/swagger.yaml');
 
 const app = express()
 
@@ -14,6 +17,9 @@ app.use(cors( {
 
 //static files
 app.use(express.static('public'));
+
+//swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //routes
 const movieRoutes = require('./routes/movieRoutes');
